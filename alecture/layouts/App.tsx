0fa389ab8469f0408bc90249bc442e4441aba713
스테판짜장 <jjiaang87@gmail.com>
2022-02-05 04:51:07 +0000

    babel and webpack setting

---
+++ alecture/layouts/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>좀만 참아주세요</div>
+	)
+}
+
+export default App;
--- alecture/webpack.config.ts
+++ alecture/webpack.config.ts
@@ -1,15 +1,20 @@
 import path from 'path';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import webpack from 'webpack';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack from "webpack";
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+// interface Configuration extends WebpackConfiguration {
+//   devServer?: WebpackDevServerConfiguration;
+// }
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: webpack.Configuration = {
   name: 'sleact',
   mode: isDevelopment ? 'development' : 'production',
-  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  devtool: isDevelopment ? 'hidden-source-map' : 'inline-source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
@@ -34,21 +39,13 @@ const config: webpack.Configuration = {
             [
               '@babel/preset-env',
               {
-                targets: { browsers: ['last 2 chrome versions'] },
+                targets: { browsers: ['IE 10'] },
                 debug: isDevelopment,
               },
             ],
             '@babel/preset-react',
             '@babel/preset-typescript',
-          ],
-          env: {
-            development: {
-              plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
-            },
-            production: {
-              plugins: ['@emotion'],
-            },
-          },
+          ], 
         },
         exclude: path.join(__dirname, 'node_modules'),
       },
@@ -59,12 +56,12 @@ const config: webpack.Configuration = {
     ],
   },
   plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
+    // new ForkTsCheckerWebpackPlugin({
+    //   async: false,
       // eslint: {
       //   files: "./src/**/*",
       // },
-    }),
+    // }),
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
   ],
   output: {
@@ -72,27 +69,23 @@ const config: webpack.Configuration = {
     filename: '[name].js',
     publicPath: '/dist/',
   },
-  devServer: {
-    historyApiFallback: true, // react router
-    port: 3090,
-    publicPath: '/dist/',
-    proxy: {
-      '/api/': {
-        target: 'http://localhost:3095',
-        changeOrigin: true,
-      },
-    },
-  },
+  // devServer: {
+  //   historyApiFallback: true,
+  //   port: 3090,
+  //   devMiddleware: { publicPath: '/dist/' },
+  //   static: { directory: path.resolve(__dirname) },
+  // },
 };
 
 if (isDevelopment && config.plugins) {
-  config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new ReactRefreshWebpackPlugin());
-  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+  // config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // config.plugins.push(new ReactRefreshWebpackPlugin({
+  //   overlay: {
+  //     useURLPolyfill: true
+  //   }
+  // }));
 }
 if (!isDevelopment && config.plugins) {
-  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
 }
 
-export default config;
+export default config; 
