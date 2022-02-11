bf49f573e41e14100547b78f68fd07c6bd9a68fd
스테판짜장 <jjiaang87@gmail.com>
2022-02-11 04:12:40 +0000

    emotion babel plugin

--- alecture/webpack.config.ts
+++ alecture/webpack.config.ts
@@ -48,7 +48,10 @@ const config: Configuration = {
           ],
 		  env: {
 			  development: {
-				  plugins: [require.resolve('react-refresh/babel')]
+				  plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')]
+			  },
+			  production: {
+				  plugins: ['@emotion']
 			  }
 		  }
         },
